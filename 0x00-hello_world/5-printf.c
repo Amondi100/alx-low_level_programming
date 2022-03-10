64ebf9761f1cd49956ed626bfa3e24b3cbfc0bc7
Amondi100 <99266559+Amondi100@users.noreply.github.com>
2022-03-10 13:17:16 -0800

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
- * 	* description: main - Prints out a string to stdout.
- * 	*return: 0 if success
- */
+ *  * main - starting point of program
+ *    * Return: return 0 if successful
+*/
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art.\n");
-	return (0);
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+			return (0);
 }
