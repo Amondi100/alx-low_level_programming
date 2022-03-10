d7747eaee306047cf8fc2675a1d21c06085a52de
Amondi100 <99266559+Amondi100@users.noreply.github.com>
2022-03-10 12:12:06 -0800

    C puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 
-	/**
-	 * main - Entry point
-	 *
-	 * Return: Always 0 (Success)
-	 */
+/**
+ *	* description: main - prints out a string to stdout
+ *	* Return: 0 if success
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
