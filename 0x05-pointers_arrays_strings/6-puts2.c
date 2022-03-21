7263ef4b3c53ade0224db33e313878571774a9d8
Amondi100 <99266559+Amondi100@users.noreply.github.com>
2022-03-21 12:25:50 -0700

    string

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,17 +6,23 @@
  */
 void puts2(char *str)
 {
-	int i, j = 0;
+	int len = 0;
+	int l = 0;
+	char *y = str;
+	int z;
 
-	while (str[i] != '\0')
+	while (*y != '\0')
 	{
-		i++;
+		y++;
+		len++;
 	}
-
-	while (j < i)
+	l = len - 1;
+	for (z = 0 ; z <= l ; z++)
 	{
-		_putchar(str[j]);
-		j = j + 2;
+		if (z % 2 == 0)
+		{
+			_putchar(str[z]);
+		}
 	}
 	_putchar('\n');
 }
