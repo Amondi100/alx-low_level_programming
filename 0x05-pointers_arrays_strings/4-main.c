2c90144d15513912afab4518e34c7747e12ed0c1
Amondi100 <99266559+Amondi100@users.noreply.github.com>
2022-03-21 06:15:49 -0700

    check out

---
+++ 0x05-pointers_arrays_strings/4-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ * Return: always 0.
+ */
+int main(void)
+{
+	char *str;
+	str = "Ido not fear computers, i fear the lack of them - isaa asimov";
+	print_rev(str);
+	return (0);
+}
