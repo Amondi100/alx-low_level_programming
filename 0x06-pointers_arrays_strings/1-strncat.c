7014f570652bfe37a59d206dd6997dba2596f561
Amondi100 <99266559+Amondi100@users.noreply.github.com>
2022-03-24 04:07:20 -0700

    string concatenation n

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strncat - function to concatenate 2 strings.
+ * @dest: parameter
+ * @src: parameter
+ * @n: int parameter.
+ * Return: *dest.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int m;
+	int i;
+
+	m = 0;
+
+	for (i = 0; i < 1000; i++)
+	{
+		if (dest[i] == '\0')
+		{
+			break;
+		}
+		m++;
+	}
+
+	for (i = 0; src[i] != '\0' && i < n; i++)
+	{
+		dest[m + i] = src[i];
+	}
+	dest[m + i] = '\0';
+	return (dest);
+}
