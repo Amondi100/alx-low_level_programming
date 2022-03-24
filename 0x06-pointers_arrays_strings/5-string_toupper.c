508c72bfdc3854161dbfe674daf8d29da8909096
Amondi100 <99266559+Amondi100@users.noreply.github.com>
2022-03-24 06:24:14 -0700

    toupper function

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -10,12 +10,11 @@ char *string_toupper(char *n)
 {
 	int i;
 
-	i = 0;
-	while (n[i] != '\0')
+	for (i = 0; n[i] != '\0'; i++)
 	{
-		if (n[i] >= 'a' && n[i] <= 'Z')
-			n[i] = n[i] - 32;
-		i++;
+		if (n[i] >= 'a' && n[i] <= 'z')
+			n[i] -= 32;
 	}
+
 	return (n);
 }
