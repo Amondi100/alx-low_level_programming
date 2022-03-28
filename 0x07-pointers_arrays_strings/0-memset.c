035ec68a0fde26fca750f53ee03c96f6855059df
Amondi100 <99266559+Amondi100@users.noreply.github.com>
2022-03-28 10:23:40 -0700

    header

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _memset - fills memory with a constant byte
+ * @s: pointed destination.
+ * @n: bytes.
+ * @b: constant byte.
+ * Return: s if success.
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+	return (s);
+}
