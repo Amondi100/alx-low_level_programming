b9676fb1290257a3d089e54cde831e83a7fe3e28
Amondi100 <99266559+Amondi100@users.noreply.github.com>
2022-03-28 14:25:23 -0700

    pointers and arrays

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_chessboard - prints the chessboard
@@ -13,8 +14,13 @@ void print_chessboard(char (*a)[8])
 	{
 		for (j = 0; j < 8; j++)
 		{
-			_putchar(a[i][j]);
+			if (j == 7)
+			{
+				putchar(a[i][j]);
+				putchar('\n');
+			}
+			else
+				putchar(a[i][j]);
 		}
-			_putchar('\n');
 	}
 }
